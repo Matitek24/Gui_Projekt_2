c318eaa6ecee237f5ff9cc9427f8a4da8686bae4
Mateusz Skrzypek <k.skrzypek1234@gmail.com>
2025-05-26 20:44:26 +0200

    Projekt Before Factory Button

---
+++ src/Services/DzialActions.java
@@ -0,0 +1,22 @@
+package Services;
+
+import Interface.EntityActions;
+import View.CenterPanel;
+
+public class DzialActions implements EntityActions {
+    private final CenterPanel centerPanel;
+    public DzialActions(CenterPanel centerPanel) {
+        this.centerPanel = centerPanel;
+    }
+
+    @Override
+    public void onAdd() {
+
+    }
+    public void onDelete() {
+
+    }
+    public void onEdit() {
+
+    }
+}
